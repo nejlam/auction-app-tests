478dc6cb6ea2b4f41335a5cb1a395e386d13646b
nejlam <nejla.maksumic@gmail.com>
2020-11-18 12:09:56 +0100

    minor changes

--- src/main/java/page_objects/auction_app/ItemPage.java
+++ src/main/java/page_objects/auction_app/ItemPage.java
@@ -257,11 +257,13 @@ public class ItemPage extends PageBase {
 
     public ItemPage checkMsgAndPlaceBid() throws InterruptedException {
         System.out.println("Msg before loop:" + extractEnterPriceMsg());
-        if(getEnterPriceMsg().getText().equals("Enter $0 or more")) {
+        if(extractEnterPriceMsg().equals("Enter $0 or more")) {
             System.out.println("Msg before wait:" + getEnterPriceMsg().getText());
-            Thread.sleep(10000);
+            synchronized (getDriver()){
+                wait(10000);
+            }
             System.out.println("Msg after wait:" + getEnterPriceMsg().getText());
-        } placeBid(getEnterPriceMsg().getText());
+        } placeBid(extractEnterPriceMsg());
     return this;
     }
 
